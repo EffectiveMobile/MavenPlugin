2e0147ea6bed55059f5886769ecfb0d0d4491694
vahzatsaava <vazhatsaava@gmail.com>
2025-01-08 13:36:58 +0400

    08.01.2025

--- MavenPlugin/src/main/java/org/plugin/DeveloperInfoMojo.java
+++ MavenPlugin/src/main/java/org/plugin/DeveloperInfoMojo.java
@@ -1,4 +1,36 @@
 package org.plugin;
 
-public class DeveloperInfoMojo {
+import org.apache.maven.model.Developer;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+
+import java.util.List;
+
+@Mojo(name = "infoDevs",defaultPhase = LifecyclePhase.COMPILE)
+public class DeveloperInfoMojo extends AbstractMojo {
+    @Parameter(defaultValue = "${project}", readonly = true)
+    private MavenProject mavenProject;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        List<Developer> developers = mavenProject.getDevelopers();
+
+        if (developers.isEmpty()) {
+            getLog().info("No developers found");
+        } else {
+            for (Developer developer : developers) {
+                getLog().info("Dev id: " + developer.getId());
+                getLog().info("Name: " + developer.getName());
+                getLog().info("Email: " + developer.getEmail());
+                getLog().info("Organization: " + developer.getOrganization());
+                getLog().info("Roles: " + developer.getRoles());
+                getLog().info("TimeZone: " + developer.getTimezone());
+            }
+        }
+    }
 }
