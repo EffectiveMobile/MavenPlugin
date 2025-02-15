358e8e5114b45107916822829a6a751875b15a9e
vsirik <vadimss.dremin@gmail.com>
2025-02-15 21:11:58 +0300

    Maven plugin

---
+++ MavenPlugin/src/main/java/org/plugin/Constants.java
@@ -0,0 +1,9 @@
+package org.plugin;
+
+public class Constants {
+
+    public static final String SPACE = " ";
+    public static final String EMPTY = "";
+    public static final String SPLITTER = "------------------------------";
+
+}
